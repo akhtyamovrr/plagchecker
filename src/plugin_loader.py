bef62c33143d665508b4bc8b03ad6b40aed47c27
akhtyamovrr <akhtyamov.27@mail.ru>
2016-04-23 18:56:42 +0300

    Split plugin load into two functions to increase code re-usability for future use to load single plugin

--- src/plugin_loader.py
+++ src/plugin_loader.py
@@ -3,6 +3,7 @@ import imp
 
 def load(config_file_name):
     """
+    Loading units from settings file
     :param config_file_name: name of configuration file than contains list of plugins to load. Every plugin should be
      placed at new string
     :return: list of loaded plugins. If configuration file was empty or had nonexistent plugins, returns empty list.
@@ -13,17 +14,29 @@ def load(config_file_name):
     try:
         modules = []
         for name in file.readlines():
-            name = name.rstrip('\n')
-            fp = None
-
-            try:
-                fp, pathname, description = imp.find_module(name)
-                modules.append(imp.load_module(name, fp, pathname, description))
-            except ImportError:
+            module = load_by_name(name)
+            if module is None:
                 continue
-            finally:
-                if fp:
-                    fp.close()
+            modules.append(module)
     finally:
         file.close()
     return modules
+
+
+def load_by_name(filename):
+    """
+    Loading unit by file name
+    :param filename: name of unit to load
+    :return: loaded module
+    """
+    name = filename.rstrip('\n')
+    fp = None
+    try:
+        fp, pathname, description = imp.find_module(name)
+        return imp.load_module(name, fp, pathname, description)
+    except ImportError:
+        return None
+    finally:
+        if fp:
+            fp.close()
+
