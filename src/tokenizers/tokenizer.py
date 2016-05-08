daa3ed097b081b67974b6cff3b085a2590192baa
akhtyamovrr <akhtyamov.27@mail.ru>
2016-05-08 07:42:27 +0300

    Initialized tokenizer with epydoc

---
+++ src/tokenizers/tokenizer.py
@@ -0,0 +1,9 @@
+
+def convert(map, source_string):
+    """
+    Converts source string of program to token string
+    :param map: dictionary of operator replacements
+    :param source_string: string of program for conversion
+    :return: tokenized representation of source program
+    """
+    return None
